cdb0a7416e45ee0483392619955eda034d45bba2
Joey Cherisea <hai.p@northeastern.edu>
2025-05-06 23:02:13 -0700

    Register trajectory app in project setting

--- PaLMTo_App/PaLMTo_App/settings.py
+++ PaLMTo_App/PaLMTo_App/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trajectory.apps.TrajectoryConfig',     # Add main trajectory app
 ]
 
 MIDDLEWARE = [
