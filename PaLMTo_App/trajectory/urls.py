7730dadb61de7f541773cdfd9fee1f38dac8bbc8
Joey Cherisea <hai.p@northeastern.edu>
2025-05-06 23:07:27 -0700

    Clear url and view file in trajectory app

--- PaLMTo_App/trajectory/urls.py
+++ PaLMTo_App/trajectory/urls.py
@@ -1,10 +1,1 @@
-from django.urls import path
-from . import views
 
-app_name = 'trajectory'
-
-urlpatterns = [
-    # Add your API endpoints here
-    path('api/trajectory/', views.trajectory_list, name='trajectory_list'),
-    path('api/trajectory/<int:pk>/', views.trajectory_detail, name='trajectory_detail'),
-]
