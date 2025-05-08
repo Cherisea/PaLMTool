da01e2fbea8c6aef262cbf557867562365c7e247
Joey Cherisea <hai.p@northeastern.edu>
2025-05-08 13:41:06 -0700

    Register trajectory tables with admin site

--- PaLMTo_App/trajectory/admin.py
+++ PaLMTo_App/trajectory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UploadedTrajectory, GeneratedTrajectory
 
 # Register your models here.
+@admin.register(UploadedTrajectory)
+def UploadedTrajectoryAdmin():
+    list_display = ('id', 'uploaded_at', 'original_file')
+    search_fields = ('original_file',)
+    # Listed in descending order
+    ordering = ('-uploaded_at')
+
+@admin.register(GeneratedTrajectory)
+def GeneratedTrajectoryAdmin():
+    list_display = ('id', 'uploaded', 'generation_method', 'generated_file', 'created_at')
+    list_filter = ('generation_method', 'created_at')
+    search_fields = ('generated_file', )
+    ordering = ('-created_at', )
+
