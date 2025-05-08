df74ec7e5033b3201686c93ad24fb7c030f6d4b4
Joey Cherisea <hai.p@northeastern.edu>
2025-05-08 13:44:34 -0700

    Fix a bug where AdminModel class was incorrectly defined as function

--- PaLMTo_App/trajectory/admin.py
+++ PaLMTo_App/trajectory/admin.py
@@ -3,14 +3,14 @@ from .models import UploadedTrajectory, GeneratedTrajectory
 
 # Register your models here.
 @admin.register(UploadedTrajectory)
-def UploadedTrajectoryAdmin():
+class UploadedTrajectoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'uploaded_at', 'original_file')
     search_fields = ('original_file',)
     # Listed in descending order
     ordering = ('-uploaded_at')
 
 @admin.register(GeneratedTrajectory)
-def GeneratedTrajectoryAdmin():
+class GeneratedTrajectoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'uploaded', 'generation_method', 'generated_file', 'created_at')
     list_filter = ('generation_method', 'created_at')
     search_fields = ('generated_file', )
