93356883c50df6a9fa919d86e692c54f2f2b141b
Joey Cherisea <hai.p@northeastern.edu>
2025-05-09 18:18:13 -0700

    Add a new model that stores user-selected configuration for trajectory generation

--- PaLMTo_App/trajectory/models.py
+++ PaLMTo_App/trajectory/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Table for uploaded file
 class UploadedTrajectory(models.Model):
     """
         Database table to store user uploaded csv files. 
@@ -12,15 +12,33 @@ class UploadedTrajectory(models.Model):
     def __str__(self) -> str:
         return f"Uploaded trajectory {self.id} ({'default file' if not self.original_file else self.original_file.name})"
     
+
+# Table for user-configed options
+class GenerationConfig(models.Model):
+    uploaded = models.ForeignKey(UploadedTrajectory, on_delete=models.CASCADE, related_name='generation_configs')
+    cell_size = models.IntegerField(help_text="Grid cell size in meters")
+    num_trajectories = models.PositiveIntegerField(help_text="Number of trajectories to generate")
+    trajector_length = models.PositiveIntegerField(
+        blank=True, null=True,
+        help_text="Length of each trajectory, if applicable."
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    generation_method = models.CharField(max_length=50, choices=[
+        ('length_constrained', 'Length-Constrained from Start Point'),
+        ('point_to_point', 'Between Two Points')
+    ])
+
+    def __str__(self):
+        return f"Config for Upload {self.uploaded.id} ({self.generation_method})"
+
+
+# Table for generated trajectories
 class GeneratedTrajectory(models.Model):
     # Create many-to-one relatioinship 
     uploaded = models.ForeignKey(UploadedTrajectory, on_delete=models.CASCADE, related_name='generated_files')
+    config = models.ForeignKey(GenerationConfig, on_delete=models.CASCADE, related_name='generated_trajectories')
     generated_file = models.FileField(upload_to='generated/')
     created_at = models.DateTimeField(auto_now_add=True)
-    generation_method = models.CharField(max_length=50, choices=[
-        ('length_constrained', 'Length Constrained'),
-        ('point_to_point', 'Between Given Points')
-    ])
 
     def __str__(self) -> str:
         return f"GeneratedTrajectory {self.id} from Upload {self.uploaded.id}"
