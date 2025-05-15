6126b65de0e4fdbc3d6205d59b3014bb67ae8193
Joey Cherisea <hai.p@northeastern.edu>
2025-05-15 10:28:14 -0700

    Temporarily Change config field to optional in GeneratedTrajectory for testing

--- PaLMTo_App/trajectory/models.py
+++ PaLMTo_App/trajectory/models.py
@@ -15,7 +15,7 @@ class UploadedTrajectory(models.Model):
 
 # Table for user-configed options
 class GenerationConfig(models.Model):
-    uploaded = models.ForeignKey(UploadedTrajectory, on_delete=models.CASCADE, related_name='generation_configs')
+    uploaded = models.ForeignKey(UploadedTrajectory, on_delete=models.CASCADE, related_name='generation_configs', null=True, blank=True)
     cell_size = models.IntegerField(help_text="Grid cell size in meters")
     num_trajectories = models.PositiveIntegerField(help_text="Number of trajectories to generate")
     trajector_length = models.PositiveIntegerField(
@@ -36,7 +36,7 @@ class GenerationConfig(models.Model):
 class GeneratedTrajectory(models.Model):
     # Create many-to-one relatioinship 
     uploaded = models.ForeignKey(UploadedTrajectory, on_delete=models.CASCADE, related_name='generated_files')
-    config = models.ForeignKey(GenerationConfig, on_delete=models.CASCADE, related_name='generated_trajectories')
+    config = models.ForeignKey(GenerationConfig, on_delete=models.CASCADE, related_name='generated_trajectories', null=True, blank=True)
     generated_file = models.FileField(upload_to='generated/')
     created_at = models.DateTimeField(auto_now_add=True)
 
