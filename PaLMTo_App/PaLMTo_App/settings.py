1f73c2cefae91e0f5672b9c787f6260b045f2a84
Joey Cherisea <hai.p@northeastern.edu>
2025-05-26 12:37:47 -0700

    Configure media_url to support file download in frontend

--- PaLMTo_App/PaLMTo_App/settings.py
+++ PaLMTo_App/PaLMTo_App/settings.py
@@ -143,5 +143,8 @@ GEOJSON_DIR = BASE_DIR / 'trajectory' / 'geojson'
 # Server filepath for storing user-uploaded files
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# URL for accessing files stored on server
+MEDIA_URL = '/media/'
+
 # Size limit of uploaded files set at 100MB. Default to 2.5MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 100 * 1024 * 1024
