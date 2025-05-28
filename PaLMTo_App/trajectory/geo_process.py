0b4c44cf8a9c540e876a5218bbdfd6311341e94e
Joey Cherisea <hai.p@northeastern.edu>
2025-05-28 16:21:41 -0700

    Add a function that extracts boundary from sample trajectory file

--- PaLMTo_App/trajectory/geo_process.py
+++ PaLMTo_App/trajectory/geo_process.py
@@ -5,7 +5,9 @@ import requests
 import json
 import os
 from django.conf import settings
-
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Polygon
 
 NOMINATIM_API = "https://nominatim.openstreetmap.org/search"
 
@@ -69,4 +71,39 @@ def get_city_boundary(city, country=None, save_dir=settings.STATIC_ROOT):
         return filename
     else:
         return f"No administrative boundary found for {city}. Raw result {results}"
+
+def extract_boundary(df):
+    """
+        Extract geographical boundary of an area from a Pandas dataframe.
+
+        df: dataframe containing a geometry column with a list of lists in each row
+
+        Return a GeoDataFrame with pairs of coordinates representing the boundary of an area
+    """
+    # Initialize coordinates of bottom left and top right points of an area
+    sw = {'log': float('inf'), 'lat': float('inf')}
+    ne = {'log': float('-inf'), 'lat': float('-inf')}
+
+    # Update longitude and latitude of both points
+    for coords in df['geometry']:
+        for lon, lat in coords:
+            if lon < sw['log']:
+                sw['log'] = lon
+            if lon > ne['log']:
+                ne['log'] = lon
+            if lat < sw['lat']:
+                sw['lat'] = lat
+            if lat > ne['lat']:
+                ne['lat'] = lat
+
+    # Construct a rectangle-shaped polygon delimiting its boundary
+    sw_coords = [sw['log'], sw['lat']]
+    ne_coords = [ne['log'], ne['lat']]
+    nw_coords = [sw['log'], ne['lat']]
+    se_coords = [ne['log'], sw['lat']]
+
+    boundary = [sw_coords, nw_coords, ne_coords, se_coords, sw_coords]
+    poly = Polygon(boundary)
+
+    return gpd.GeoDataFrame(pd.DataFrame([{"geometry": poly}]), geometry='geometry')    
 
