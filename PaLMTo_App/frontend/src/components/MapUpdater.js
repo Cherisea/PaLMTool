741f5c3908d3c60370c6a90f831cb276a8c1e150
Joey Cherisea <hai.p@northeastern.edu>
2025-05-31 16:20:04 -0700

    Improve code readibility by adding more comments

--- PaLMTo_App/frontend/src/components/MapUpdater.js
+++ PaLMTo_App/frontend/src/components/MapUpdater.js
@@ -1,12 +1,17 @@
+/*
+  This JS function updates map view whenever map center is changed or map instance is recreated.
+*/
+
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 
 function MapUpdater({ center }) {
+  // Retrieve leaflet map instance 
   const map = useMap();
 
   useEffect(() => {
-    map.setView(center);
-  }, [center, map]);
+    map.setView(center);   // Move map view to new center
+  }, [center, map]);  // Dependency array
 
   return null;
 }
