54de68cc62c6f9fe35be6b14ac017fce1c2ec007
Joey Cherisea <hai.p@northeastern.edu>
2025-06-03 11:03:00 -0700

    Add a new url mapping for heatmap visualization

--- PaLMTo_App/trajectory/urls.py
+++ PaLMTo_App/trajectory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GenerationConfigView, download_files
+from .views import GenerationConfigView, download_files, compare_trajectory_heatmap
 
 
 urlpatterns = [
     path('generate/', GenerationConfigView.as_view(), name='generate'),
+    path('compare/', compare_trajectory_heatmap, name='compare_trajectory'),
     path('download/<str:filename>', download_files, name='download_files')
 ]
 
