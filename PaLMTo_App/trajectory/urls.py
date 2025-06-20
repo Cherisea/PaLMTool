dc7f8ff1484e71af9b2fd487e34df96a8e21246b
Joey Cherisea <hai.p@northeastern.edu>
2025-06-19 17:57:00 -0700

    Add a url mapping for 3D trajectory view

--- PaLMTo_App/trajectory/urls.py
+++ PaLMTo_App/trajectory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GenerationConfigView, download_files, compare_trajectory_heatmap
+from .views import GenerationConfigView, download_files, compare_trajectory_heatmap, Trajectory3DView
 
 
 urlpatterns = [
     path('generate/', GenerationConfigView.as_view(), name='generate'),
     path('download/<str:filename>', download_files, name='download_files'),
+    path('analyze', Trajectory3DView.as_view(), name="analyze")
 ]
 
