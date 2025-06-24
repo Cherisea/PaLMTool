0eca58f5f97a06cf8c3e8ed87ac061718f9f2da7
Joey Cherisea <hai.p@northeastern.edu>
2025-06-24 15:24:01 -0700

    Add a url mapping to patterns

--- PaLMTo_App/trajectory/urls.py
+++ PaLMTo_App/trajectory/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GenerationConfigView, download_files, Trajectory3DView
+from .views import GenerationConfigView, download_files, Trajectory3DView, MapMatchingView
 
 
 urlpatterns = [
     path('generate/', GenerationConfigView.as_view(), name='generate'),
     path('download/<str:filename>', download_files, name='download_files'),
-    path('analyze', Trajectory3DView.as_view(), name="analyze")
+    path('analyze/', Trajectory3DView.as_view(), name="analyze"),
+    path('map-match/', MapMatchingView.as_view(), name="map_match")
 ]
 
