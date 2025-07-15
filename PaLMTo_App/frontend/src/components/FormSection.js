2401af9e37bfdd55c15681cc8191ade780a5544e
Joey Cherisea <hai.p@northeastern.edu>
2025-07-14 17:56:26 -0700

    Wrap form in a new div class

--- PaLMTo_App/frontend/src/components/FormSection.js
+++ PaLMTo_App/frontend/src/components/FormSection.js
@@ -120,136 +120,138 @@ function FormSection({
         </div>
       </div>
 
-      <form onSubmit={handleFormSubmit} encType='multipart/form-data'>
-      {/* STEP 1: model training */}
-      {currentStep === 1 && (
-        <div className="step-contet">
-          <div className="form-group">
-              <label className="required">
-                Sample Trajectory
-                <span className="required-mark">*</span>
-                <FiInfo title="Sample trajectory which synthetic trajectories will be generated from. Must be a csv file with three columns: 'trip_id', 'timestamp', 'geometry'" className="info-icon"/>
-              </label>
-              <div {...getRootProps({ className: "dropzone" })}>
-                <input {...getInputProps()} />
-                {
-                  isDragActive ? 
-                    (<p>Drop the file here ...</p>) : 
-                    formData.file ? 
-                      (<p>{formData.file.name}</p>) : 
-                      (<p>Drag & drop a file here, or click to select one</p>)
-                }
+      <div className="form-content">
+        <form onSubmit={handleFormSubmit} encType='multipart/form-data'>
+        {/* STEP 1: model training */}
+        {currentStep === 1 && (
+          <div className="step-contet">
+            <div className="form-group">
+                <label className="required">
+                  Sample Trajectory
+                  <span className="required-mark">*</span>
+                  <FiInfo title="Sample trajectory which synthetic trajectories will be generated from. Must be a csv file with three columns: 'trip_id', 'timestamp', 'geometry'" className="info-icon"/>
+                </label>
+                <div {...getRootProps({ className: "dropzone" })}>
+                  <input {...getInputProps()} />
+                  {
+                    isDragActive ? 
+                      (<p>Drop the file here ...</p>) : 
+                      formData.file ? 
+                        (<p>{formData.file.name}</p>) : 
+                        (<p>Drag & drop a file here, or click to select one</p>)
+                  }
+                </div>
+                <p className="note">
+                  <i>
+                    Note: Use <a href={`${process.env.REACT_APP_API_URL}/trajectory/download/demo.csv`} download>demo trajectory file</a> by leaving this field blank.
+                  </i>
+                </p>
               </div>
-              <p className="note">
-                <i>
-                  Note: Use <a href={`${process.env.REACT_APP_API_URL}/trajectory/download/demo.csv`} download>demo trajectory file</a> by leaving this field blank.
-                </i>
-              </p>
-            </div>
 
+              <div className="form-group">
+                <label className="required">
+                  Cell Size
+                  <span className="required-mark">*</span>
+                  <FiInfo title="Size of each grid cell in meters representing a geological location over which trajectories are to be generated" className="info-icon"/>
+                </label>
+                <input 
+                  name="cell_size" 
+                  type="number" 
+                  value={formData.cell_size} 
+                  onChange={handleChange} 
+                  step="50"
+                  min="50"
+                  required 
+                />
+              </div>
+
+              <div className="button-container">
+                <button 
+                  type="button"
+                  onClick={nextStep}
+                  disabled={!canProceed()}
+                  className="next-button"
+                >
+                  Next
+                </button>
+              </div>
+          </div>
+        )}
+
+        {/* STEP 2: additional parameters for trajectory generation */}
+        {currentStep === 2 && (
+          <div className="step-content">
             <div className="form-group">
               <label className="required">
-                Cell Size
+                Number of Generated Trajectories
                 <span className="required-mark">*</span>
-                <FiInfo title="Size of each grid cell in meters representing a geological location over which trajectories are to be generated" className="info-icon"/>
+                <FiInfo title="Quantity of new trajectories to be generated" className="info-icon"/>
               </label>
               <input 
-                name="cell_size" 
+                name="num_trajectories" 
                 type="number" 
-                value={formData.cell_size} 
-                onChange={handleChange} 
-                step="50"
-                min="50"
+                value={formData.num_trajectories} 
+                onChange={handleChange}
+                step="100"
+                min="100"
                 required 
               />
             </div>
 
-            <div className="button-container">
-              <button 
-                type="button"
-                onClick={nextStep}
-                disabled={!canProceed()}
-                className="next-button"
+            <div className="form-group">
+              <label className="required">
+                Generation Method <span className="required-mark">*</span>
+                <FiInfo 
+                  title="Approach via which trajectories should be generated. 'Length-constrained' method generates new
+                  trajectories with a predefined length; 'Point-to-Point' fills the gap between two bigrams" 
+                  className="info-icon"
+                />
+              </label>
+              <select 
+                name="generation_method" 
+                value={formData.generation_method} 
+                onChange={handleChange} 
+                required
               >
-                Next
-              </button>
+                <option value="">Select a Method</option>
+                <option value="length_constrained">Length Constrained</option>
+                <option value="point_to_point">Point to Point</option>
+              </select>
             </div>
-        </div>
-      )}
 
-      {/* STEP 2: additional parameters for trajectory generation */}
-      {currentStep === 2 && (
-        <div className="step-content">
-          <div className="form-group">
-            <label className="required">
-              Number of Generated Trajectories
-              <span className="required-mark">*</span>
-              <FiInfo title="Quantity of new trajectories to be generated" className="info-icon"/>
-            </label>
-            <input 
-              name="num_trajectories" 
-              type="number" 
-              value={formData.num_trajectories} 
-              onChange={handleChange}
-              step="100"
-              min="100"
-              required 
-            />
-          </div>
-
-          <div className="form-group">
-            <label className="required">
-              Generation Method <span className="required-mark">*</span>
-              <FiInfo 
-                title="Approach via which trajectories should be generated. 'Length-constrained' method generates new
-                trajectories with a predefined length; 'Point-to-Point' fills the gap between two bigrams" 
-                className="info-icon"
+            <div className="form-group">
+              <label>
+                Trajectory Length <em>(optional)</em>
+                <FiInfo title="Number of points in a generated trajectory. Not applicable for point-to-point trajectory generation" className="info-icon"/>
+              </label>
+              <input 
+                name="trajectory_len" 
+                type="number" 
+                value={formData.trajectory_len} 
+                onChange={handleChange} 
+                step="100"
+                min="100"
+                disabled={formData.generation_method === "point_to_point" || formData.generation_method === ""}
+                className={formData.generation_method === "point_to_point" ? "disabled-input" : ""}
+                required={formData.generation_method !== "point_to_point"}
               />
-            </label>
-            <select 
-              name="generation_method" 
-              value={formData.generation_method} 
-              onChange={handleChange} 
-              required
-            >
-              <option value="">Select a Method</option>
-              <option value="length_constrained">Length Constrained</option>
-              <option value="point_to_point">Point to Point</option>
-            </select>
-          </div>
+            </div>
 
-          <div className="form-group">
-            <label>
-              Trajectory Length <em>(optional)</em>
-              <FiInfo title="Number of points in a generated trajectory. Not applicable for point-to-point trajectory generation" className="info-icon"/>
-            </label>
-            <input 
-              name="trajectory_len" 
-              type="number" 
-              value={formData.trajectory_len} 
-              onChange={handleChange} 
-              step="100"
-              min="100"
-              disabled={formData.generation_method === "point_to_point" || formData.generation_method === ""}
-              className={formData.generation_method === "point_to_point" ? "disabled-input" : ""}
-              required={formData.generation_method !== "point_to_point"}
-            />
+            <div className="button-container">
+              <button type='submit' disabled={isLoading}>
+                {isLoading ? 'Generating...' : 'Generate'}
+              </button>
+              {isLoading && (
+                <div className="progress-container">
+                  <div className="progress-bar" style={{ width: `${progress}%` }} />
+                </div>
+              )}
+            </div>
           </div>
+        )}
 
-          <div className="button-container">
-            <button type='submit' disabled={isLoading}>
-              {isLoading ? 'Generating...' : 'Generate'}
-            </button>
-            {isLoading && (
-              <div className="progress-container">
-                <div className="progress-bar" style={{ width: `${progress}%` }} />
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-
-      </form>
+        </form>
+      </div>
 
       <DocLinks />
     </div>
