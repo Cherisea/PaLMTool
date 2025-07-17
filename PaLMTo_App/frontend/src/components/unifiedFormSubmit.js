a90ff014d6f872a4a44da8572a0c762905c097a1
Joey Cherisea <hai.p@northeastern.edu>
2025-07-17 14:49:27 -0700

    Create a separate script to handle form submit

---
+++ PaLMTo_App/frontend/src/components/unifiedFormSubmit.js
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import axios from "axios";
+
+function UnifiedFormSubmit(formData, setCurrentStep, setShowStats, setStatsData) {
+    // State variable for notification message
+  const [notification, setNotification] = useState(null);
+
+  // State variable for loading status
+  const [isLoading, setIsLoading] = useState(false);
+
+  // State variable for current progress
+  const [progress, setProgress] = useState(0);
+
+  // Handler of API calls
+  const submitFormData = async (endpoint, payload) => {
+    return await axios.post(endpoint, payload, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+  };
+
+  const handleUnifiedSubmit = async (e, currentStep) => {
+    e.preventDefault();
+
+    if (currentStep === 1) {
+        setCurrentStep(2)
+        return;
+    }
+
+    setIsLoading(true);
+    setProgress(0);
+  
+    // Simulate progress update
+    const progressInterval = setInterval(() => {
+        setProgress((prevProgress) => {
+        if (prevProgress >= 90) {
+            clearInterval(progressInterval);
+            return 90;
+        }
+        return prevProgress + 10;
+        })
+    }, 1100);
+
+    try {
+        let endpoint, payload;
+
+        // Build ngram dict only in step 2 and csv file is uploaded
+        if (currentStep === 2 && !!formData.file) {
+            endpoint = '/trajectory/generate/ngrams';
+            payload = new FormData();
+            payload.append("cell_size", formData.cell_size);
+            payload.append("file", formData.file);
+        } else if (currentStep === 3) {
+            endpoint = '/trajectory/generate/';
+            payload = new FormData();
+            payload.append("num_trajectories", formData.num_trajectories);
+            payload.append("generation_method", formData.generation_method);
+            payload.append("ngram_file", formData.ngram_file);
+
+            if (formData.generation_method !== "point_to_point" && formData.trajectory_len) {
+                payload.append("trajectory_len", formData.trajectory_len);
+            }
+        }
+
+        if (endpoint && payload) {
+            const response = await submitFormData(endpoint, payload);
+
+            setProgress(100);
+
+            if (currentStep === 2) {
+                setNotification({
+                    type: 'success',
+                    message: 'Ngram dictionaries created successfully!'
+                  });
+                setStatsData(response.data.stats);
+                setShowStats(true);
+                setCurrentStep(3);
+            } else if (currentStep === 3) {
+                setTimeout(() => {
+                    setNotification({
+                        type: 'success',
+                        message: 'Trajectories generated successfully!'
+                      });
+                }, 1000);
+            }
+        }
+    } catch (error) {
+        setNotification({
+            type: 'error',
+            message: currentStep === 2
+              ? 'Failed to create ngram dictionaries. Please try again.'
+              : 'Failed to generate trajectories. Please try again.'
+        });
+    } finally {
+        clearInterval(progressInterval);
+        setIsLoading(false);
+    }
+
+   };
+
+   return {
+    handleUnifiedSubmit,
+    notification,
+    isLoading,
+    progress,
+    setNotification
+   };
+
+}
+
+export default UnifiedFormSubmit;
