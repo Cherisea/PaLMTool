fd7a249348df8f51bde1d5db4b7319a5d2853237
Joey Cherisea <hai.p@northeastern.edu>
2025-07-17 09:58:30 -0700

    Insert a URL mapping to ngram generation view

--- PaLMTo_App/trajectory/urls.py
+++ PaLMTo_App/trajectory/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import GenerationConfigView, download_files, Trajectory3DView, MapMatchingView
+from .views import GenerationConfigView, download_files, Trajectory3DView, MapMatchingView, NgramGenerationView
 
 
 urlpatterns = [
     path('generate/', GenerationConfigView.as_view(), name='generate'),
-    path('generate/ngrams', GenerationConfigView.as_view({'post': 'post_ngrams'}), name='generate_ngrams'),
+    path('generate/ngrams', NgramGenerationView.as_view(), name='generate_ngrams'),
     path('download/<str:filename>', download_files, name='download_files'),
     path('analyze/', Trajectory3DView.as_view(), name="analyze"),
     path('map-match/', MapMatchingView.as_view(), name="map_match")
