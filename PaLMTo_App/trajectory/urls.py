716840584325c2b550f6516550967e944248b6c8
Joey Cherisea <hai.p@northeastern.edu>
2025-07-16 21:33:12 -0700

    Insert a URL mapping from a new endpoint to a new handler

--- PaLMTo_App/trajectory/urls.py
+++ PaLMTo_App/trajectory/urls.py
@@ -4,6 +4,7 @@ from .views import GenerationConfigView, download_files, Trajectory3DView, MapMa
 
 urlpatterns = [
     path('generate/', GenerationConfigView.as_view(), name='generate'),
+    path('generate/ngrams', GenerationConfigView.as_view({'post': 'post_ngrams'})),
     path('download/<str:filename>', download_files, name='download_files'),
     path('analyze/', Trajectory3DView.as_view(), name="analyze"),
     path('map-match/', MapMatchingView.as_view(), name="map_match")
