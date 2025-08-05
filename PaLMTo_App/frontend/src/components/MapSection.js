5aa92abbb0fa835115961592b1033be4ec17441e
Joey Cherisea <hai.p@northeastern.edu>
2025-08-04 20:41:01 -0700

    Move 3D view before map-matching view

--- PaLMTo_App/frontend/src/components/MapSection.js
+++ PaLMTo_App/frontend/src/components/MapSection.js
@@ -5,7 +5,7 @@ import { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 import { FiDownload } from "react-icons/fi";
 import MapMatchInputModal from "./mapMatchInput";
-import Trajectory3DViewer from "./Trajectory3DViewer";
+import Trajectory3DViewer from "./trajectory3DViewer";
 
 const LocationSelectionMap = ({ mapCenter, locationCoordinates, onLocationSelect }) => (
   <div className="map-container">
@@ -222,17 +222,18 @@ function MapSection({ mapCenter, locationCoordinates, onLocationSelect, visualDa
         color: '#ff9800'
       },
       {
-        id: 'map-matching',
-        title: 'Map-Matching',
-        available: !!mapMatchData || mapMatchLoading,
-        color: '#4caf50'
-      },
-      {
         id: '3d',
         title: '3D View',
         available: !!visualData,
         color: '#9c27b0'
+      },
+      {
+        id: 'map-matching',
+        title: 'Map-Matching',
+        available: !!mapMatchData || mapMatchLoading,
+        color: '#4caf50'
       }
+      
     ]; 
 
     return (
